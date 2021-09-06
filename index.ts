93689a5c68d1370f9775697c946554f28c02ce7d
mick-roper <mick@theropers.co.uk>
2021-09-06 15:53:43 +0100

    implemented the client
    Signed-off-by: mick-roper <mick@theropers.co.uk>

---
+++ index.ts
@@ -0,0 +1,71 @@
+import * as t from './types'
+
+export class Client {
+    private apiHost: string
+    private token: string
+    private org: string
+
+    constructor(org: string, token: string, apiHost: string = 'api.reliably.com') {
+        this.org = org
+        this.token = token
+        this.apiHost = apiHost
+    }
+
+    public async getObjectives(): Promise<t.Objective[]> {
+        let url = `https://${this.apiHost}/entities/${this.org}/reliably.com/v1/objective`
+        let req = new Request(url, {
+            headers: {
+                'Authorization': `Bearer ${this.token}`
+            }
+        })
+
+        let res = await fetch(req)
+
+        if (!res.ok) {
+            throw new Error(`HTTP error: ${res.statusText}`)
+        }
+
+        let body: [] = await res.json()
+
+        return body.map((x: any): t.Objective => ({
+            metadata: x.metadata,
+            spec: x.spec
+        }))
+    }
+
+    public async putIndicator(indicator: t.Indicator) {
+        let url = `https://${this.apiHost}/entities/${this.org}/reliably.com/v1/indicator`
+
+        let req = new Request(url, {
+            method: 'PUT',
+            body: JSON.stringify(indicator),
+            headers: {
+                'Authorization': `Bearer ${this.token}`
+            }
+        })
+
+        let res = await fetch(req)
+
+        if (!res.ok) {
+            throw new Error(`HTTP error: ${res.statusText}`)
+        }
+    }
+
+    public async putEntityContext(e: t.EntityContext) {
+        let url = `https://${this.apiHost}/entities/${this.org}/reliably.com/v1/entitycontext`
+        
+        let req = new Request(url, {
+            method: 'PUT',
+            body: JSON.stringify(e),
+            headers: {
+                'Authorization': `Bearer ${this.token}`
+            }
+        })
+
+        let res = await fetch(req)
+
+        if (!res.ok) {
+            throw new Error(`HTTP error: ${res.statusText}`)
+        }
+    }
+}
--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,12 @@
 import { Labels, Metadata } from './common'
+import { EntityContext } from './entity-context'
+import { Indicator } from './indicator'
+import { Objective } from './objective'
 
-export { Labels, Metadata }
+export {
+    Labels,
+    Metadata,
+    EntityContext,
+    Indicator,
+    Objective
+}
